40c9731bc4ba89a11df9e9bd299c9472b1ec79a3
elleryq <elleryq@gmail.com>
2013-12-18 16:44:26 +0800

    Add MainWindow which inherits QMainWindow and UI.

--- lxdm-customizer.py
+++ lxdm-customizer.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""My app"""
+"""LXDM customizer"""
+from __future__ import print_function
 import sys
 import os
+try:
+    from PySide.QtGui import *
+except ImportError:
+    print("You need to install PySide.")
+from ui_customizer import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.bindEvents()
+
+    def bindEvents(self):
+        self.ui.actionE_xit.triggered.connect(self.close)
 
 
 def main():
-    pass
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
 
 if __name__ == "__main__":
     main()
