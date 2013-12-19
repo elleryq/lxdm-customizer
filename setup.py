83b89cef5ea314085cbc2b90a09a906d22cfad3d
elleryq <elleryq@gmail.com>
2013-12-19 23:45:34 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='lxdm-customizer',
+      version='1.0',
+      description='LXDM customization tool',
+      author='Yan-ren Tsai',
+      author_email='elleryq@gmail.com',
+      url='https://github.com/elleryq/lxdm-customizer',
+      packages=['lxdm_customizer_lib'],
+      )
