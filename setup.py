9e7d672aefb82e9fd364e3957093d9e6d07b3d63
elleryq <elleryq@gmail.com>
2014-01-03 11:35:54 +0800

    Update setup.py with better method to install translations.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os
 from subprocess import call
 from glob import glob
 from distutils.core import setup
+from distutils.command.build import build
+from distutils.command.install_data import install_data
 
 # Screw the MANIFEST file, it just caches out of date data and messes
 # up my builds.
@@ -9,12 +11,25 @@ mfst = os.path.join(os.path.dirname(__file__), "MANIFEST")
 if os.path.exists(mfst):
     os.unlink(mfst)
 
-# Compile translations.
-call(['lrelease', 'lxdm-customizer.pro'])
 
-translation_path = os.path.join('share', 'lxdm-customizer', 'translations')
-DATA_FILES = [(translation_path,
-              glob('i18n/*.qm')),]
+class BuildData(build):
+    def run(self):
+        build.run(self)
+
+        # Compile translations.
+        call(['lrelease', 'lxdm-customizer.pro'])
+
+
+class InstallData(install_data):
+    def run(self):
+        self.data_files.extend(self._findTranslations())
+        install_data.run(self)
+
+    def _findTranslations(self):
+        translation_path = os.path.join('share', 'lxdm-customizer',
+                                        'translations')
+        return [(translation_path, glob('i18n/*.qm'))]
+
 
 setup(name='lxdm-customizer',
       version='1.0',
@@ -25,5 +40,9 @@ setup(name='lxdm-customizer',
       scripts=['lxdm-customizer.py'],
       packages=['lxdm_customizer_lib'],
       license="GPL2",
-      data_files=DATA_FILES,
+      data_files=[
+          ('share/applications', ['data/lxdm-customizer.desktop'])],
+      cmdclass={
+          'build': BuildData,
+          'install_data': InstallData}
       )
